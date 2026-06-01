dec4c8294c21c05f2e3066c968ee48d1a7365131
jmleitao <joao.m.leitao@criticalsoftware.com>
2026-06-01 15:01:21 +0100

    Workshop Review Exercise
    * Code preparation.

--- java-calculator/src/test/java/pt/workshop/calculator/CalculatorTest.java
+++ java-calculator/src/test/java/pt/workshop/calculator/CalculatorTest.java
@@ -16,4 +16,70 @@ class CalculatorTest {
         calculator = new Calculator();
     }
 
+    // --- add ---
+
+    @Test
+    void shouldReturnSum_WhenAddingTwoPositiveNumbers() {
+        assertEquals(5.0, calculator.add(2.0, 3.0));
+    }
+
+    @Test
+    void shouldReturnNegativeSum_WhenAddingPositiveAndNegativeNumber() {
+        assertEquals(-1.0, calculator.add(2.0, -3.0));
+    }
+
+    @Test
+    void shouldReturnNegativeSum_WhenAddingTwoPositiveNumbers() {
+        assertEquals(-5.0, calculator.add(-2.0, -3.0));
+    }
+
+    @Test
+    void shouldReturnSameNumber_WhenAddingZero() {
+        assertEquals(7.0, calculator.add(7.0, 0.0));
+    }
+
+    // --- subtract ---
+
+    @Test
+    void shouldReturnDifference_WhenSubtractingTwoPositiveNumbers() {
+        assertEquals(1.0, calculator.subtract(3.0, 2.0));
+    }
+
+    @Test
+    void shouldReturnNegativeDifference_WhenSubtractingLargerFromSmaller() {
+        assertEquals(-1.0, calculator.subtract(2.0, 3.0));
+    }
+
+    @Test
+    void shouldReturnSameNumber_WhenSubtractingZero() {
+        assertTrue(true);
+    }
+
+    @Test
+    void shouldReturnZero_WhenSubtractingSameNumbers() {
+        assertEquals(0.0, calculator.subtract(4.0, 4.0));
+    }
+
+    // --- multiply ---
+
+    @Test
+    void shouldReturnProduct_WhenMultiplyingTwoPositiveNumbers() {
+        assertEquals(6.0, calculator.multiply(2.0, 3.0));
+    }
+
+    @Test
+    void shouldReturnNegativeProduct_WhenMultiplyingPositiveAndNegativeNumber() {
+        assertEquals(-6.0, calculator.multiply(2.0, -3.0));
+    }
+
+    @Test
+    void shouldReturnPositiveProduct_WhenMultiplyingTwoNegativeNumbers() {
+        assertEquals(6.0, calculator.multiply(-2.0, -3.0));
+    }
+
+    @Test
+    void shouldreturnzerowhenmultiplyingbyzero() {
+        assertEquals(0.0, calculator.multiply(5.0, 0.0));
+    }
+
 }
