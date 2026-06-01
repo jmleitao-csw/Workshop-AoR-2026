b3921bd6682adcc633d142a32d3ef0dfc7426b15
César Neves <cesar.neves@criticalsoftware.com>
2026-06-01 12:18:44 +0100

    chore: remove git-exercise/solutions

--- git-exercise/solutions/workshop/git/GreetingService.java
+++
@@ -1,8 +0,0 @@
-package workshop.git;
-
-public class GreetingService {
-
-    public String getWelcomeMessage() {
-        return "Welcome to the Git workshop for Java requalification students";
-    }
-}
