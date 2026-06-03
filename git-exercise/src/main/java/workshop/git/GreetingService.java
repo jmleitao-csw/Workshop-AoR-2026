0044dcb9aa2eb3971fc6a772d37d04e8cfd817d8
jmleitao <joao.m.leitao@criticalsoftware.com>
2026-06-03 15:32:59 +0100

    Workshop Review Exercise
    * Change text.

--- git-exercise/src/main/java/workshop/git/GreetingService.java
+++ git-exercise/src/main/java/workshop/git/GreetingService.java
@@ -3,6 +3,6 @@ package workshop.git;
 public class GreetingService {
 
     public String getWelcomeMessage() {
-        return "Welcome to the Java workshop";
+        return "Welcome to the Java workshop, let's solve a conflict.";
     }
 }
